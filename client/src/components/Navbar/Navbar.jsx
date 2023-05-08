b19173501beaa0d924f9f3f4d0283a6be6efc2b7
Millenmark <millenmark.iv@gmail.com>
2023-05-08 12:54:18 +0800

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               <Text fontWeight={"extrabold"}>EComm Web</Text>
             </Flex>
           </Link>
-          <HStack>
+          <HStack as={"nav"} spacing={4} display={{base: "none", md: "flex"}}>
             {
               links.map(link => (
                 <NavLink key={link.linkName} path={link.path}>{link.linkName}</NavLink>
@@ -95,7 +95,8 @@ const Navbar = () => {
           <Button 
             as={ReactLink} 
             to={"/registration"}
-            p={2} 
+            m={2} 
+            display={{base: "none", md: "inline-flex"}}
             fontSize={"sm"} 
             fontWeight={600}
             _hover={{bg: "orange.400"}}
@@ -115,6 +116,7 @@ const Navbar = () => {
                   <NavLink key={link.linkName} path={link.path}>{link.linkName}</NavLink>
                 ))
               }
+              <NavLink key={"sign up"} path={"/registration"}>Sign Up</NavLink>
             </Stack>
           </Box>
         ) : null
