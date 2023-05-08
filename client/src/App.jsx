03686634358297ea783693ee5d22e0d59ed71756
Millenmark <millenmark.iv@gmail.com>
2023-05-08 13:42:10 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import {Routes, Route} from "react-router-dom"
 import { Navbar } from "./components"
 import './App.css'
+import { ProductsPage } from "./pages"
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <main>
+        <Routes>
+          <Route path="/products" element={<ProductsPage/>}/>
+        </Routes>
+      </main>
     </>
   )
 }
