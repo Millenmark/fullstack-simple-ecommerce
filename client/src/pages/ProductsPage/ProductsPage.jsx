b0eb402160b6b97beeecbcb56623c8e21e525759
Millenmark <millenmark.iv@gmail.com>
2023-05-08 13:42:26 +0800

    Update ProductsPage.jsx

--- client/src/pages/ProductsPage/ProductsPage.jsx
+++ client/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,7 +1,20 @@
+import {Center, Wrap, WrapItem} from "@chakra-ui/react"
+import { products } from "../../products"
+import { ProductCard } from "../../components"
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <Wrap spacing={"2"} justify={"center"} minHeight={"100vh"}>
+      {
+        products.map((product, index) => (
+          <WrapItem key={index}>
+            <Center w={"250px"} h={"550px"} border={"2px solid red"}>
+              {product.name}
+            </Center>
+          </WrapItem>
+        ))
+      }
+    </Wrap>
   )
 }
 
