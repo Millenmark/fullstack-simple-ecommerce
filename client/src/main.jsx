cf312b23fbdb2bf0c5708f56851d01bae3a42bea
Millenmark <millenmark.iv@gmail.com>
2023-05-08 09:29:22 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter as Router } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>,
 )
