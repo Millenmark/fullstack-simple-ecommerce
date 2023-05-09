e3a47f44c07b3e4eab9f6c167d4c90609cf5e26d
Millenmark <millenmark.iv@gmail.com>
2023-05-09 15:42:19 +0800

    Update ProductCard.jsx

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,45 @@
+import {
+  Flex,
+  Circle,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  Button,
+  Tooltip,
+  Stack,
+  HStack,
+  Link,
+  Text,
+} from "@chakra-ui/react"
+import {FiShoppingCart} from "react-icons/fi"
+import { Link as ReactLink} from "react-router-dom"
+import { StarIcon } from "@chakra-ui/icons"
+import { useState } from "react"
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
-    <div>ProductCard</div>
+    <Stack 
+      p={"2"} 
+      spacing={"3px"} 
+      bg={useColorModeValue("white", "gray.800")} 
+      minWidth={"240px"} 
+      h={"450px"} 
+      borderWidth={"1px"} 
+      rounded={"lg"} 
+      shadow={"lg"} 
+      position={"relative"}
+    >
+      {
+        product.isNew && (<Circle size={"10px"} position={"absolute"} top={"2"} right={"2"} bg={"green.300"}></Circle>)
+      }
+      {
+        product.stock <= 0 && (<Circle size={"10px"} position={"absolute"} top={"2"} right={"2"} bg={"red.300"}></Circle>)
+      }
+      <Image src={product.image} alt={product.name} roundedTop={"lg"}/>
+      
+    </Stack>
   )
 }
 
