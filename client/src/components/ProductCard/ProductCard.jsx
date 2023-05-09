be1124bed2acdf1dd1554c93e89faf0606271266
Millenmark <millenmark.iv@gmail.com>
2023-05-09 20:17:21 +0800

    Update ProductCard.jsx

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import {
   Flex,
   Circle,
@@ -17,6 +18,24 @@ import {FiShoppingCart} from "react-icons/fi"
 import { Link as ReactLink} from "react-router-dom"
 import { StarIcon } from "@chakra-ui/icons"
 import { useState } from "react"
+
+const Rating = ({rating, numReviews}) => {
+  const [iconSize, setIconSize] = useState("14px")
+  return (
+    <Flex>
+      <HStack spacing={"2px"}>
+        <StarIcon icon={iconSize} w={"14px"} color={"orange.500"}/>
+        <StarIcon icon={iconSize} w={"14px"} color={rating >= 2 ? "orange.500" : "gray.200"}/>
+        <StarIcon icon={iconSize} w={"14px"} color={rating >= 3 ? "orange.500" : "gray.200"}/>
+        <StarIcon icon={iconSize} w={"14px"} color={rating >= 4 ? "orange.500" : "gray.200"}/>
+        <StarIcon icon={iconSize} w={"14px"} color={rating >= 5 ? "orange.500" : "gray.200"}/>
+      </HStack>
+      <Text fontSize={"md"} fontWeight={"bold"} ml={"4px"}>
+        {`${numReviews} ${numReviews === 1 ? "Review" : "Reviews"}`}
+      </Text>
+    </Flex>
+  )
+}
 
 const ProductCard = ({product}) => {
   return (
@@ -38,7 +57,46 @@ const ProductCard = ({product}) => {
         product.stock <= 0 && (<Circle size={"10px"} position={"absolute"} top={"2"} right={"2"} bg={"red.300"}></Circle>)
       }
       <Image src={product.image} alt={product.name} roundedTop={"lg"}/>
-      
+      <Box flex={"1"} maxH={"5"} alignItems={"baseline"}>
+        {
+          product.stock <= 0 && (
+            <Badge rounded={"full"} px={"2"} fontSize={"0.8em"} colorScheme={"red"}>Sold Out</Badge>
+          )
+        }
+
+        {
+          product.isNew && (
+            <Badge rounded={"full"} px={"2"} fontSize={"0.8em"} colorScheme={"green"}>New</Badge>
+          )
+        }
+      </Box>
+
+      <Flex mt={"1"} justifyContent={"space-between"} alignContent={"center"}>
+        <Link as={ReactLink} to={`/product/${product._id}`} pt={"2"} cursor={"pointer"}>
+          <Box fontSize={"2xl"} fontWeight={"semibold"} lineHeight={"tight"}>
+            {product.name}
+          </Box>
+        </Link>
+      </Flex>
+
+      <Flex justifyContent={"space-between"} alignContent={"center"} py={"2"}>
+        <Rating rating={product.rating} numReviews={product.numReviews}/>
+      </Flex>
+
+      <Flex justify={"space-between"}>
+        <Box fontSize={"2xl"} color={useColorModeValue("gray.800", "white")}>
+          <Box as={"span"} color={"gray.600"} fontSize={"lg"}>
+            $
+          </Box>
+          {product.price.toFixed(2)}
+        </Box>
+
+        <Tooltip label={"Add to cart"} bg={"white"} placement={"top"} color={"gray.800"}fontSize={"1.2em"}>
+          <Button variant={"ghost"} display={"flex"} disabled={product.stock <= 0}>
+            <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"}/>
+          </Button>
+        </Tooltip>
+      </Flex>
     </Stack>
   )
 }
