35a71fa7f9565c082598e7b40172182878b0bebd
Millenmark <millenmark.iv@gmail.com>
2023-05-10 14:49:31 +0800

    Created a function that connects to mongodb

---
+++ server/src/database.js
@@ -0,0 +1,20 @@
+//connection to the database
+import mongoose from "mongoose"
+
+const connectToDatabase = async () => {
+  console.log(process.env.MONGO_URI)
+
+  try {
+    //mongoose.set("strictQuery", false) use this if you have multiple versions of the mongodb and it's giving you errors
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+
+    console.log(`MongoDB Connected: ${connect.connection.host}`)
+  } catch (error) {
+    console.log(`There was an Error: ${error.message}`)
+  }
+}
+
+export default connectToDatabase
