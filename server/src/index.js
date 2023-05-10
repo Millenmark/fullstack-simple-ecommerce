9c6c7d164474232c2b0923b43d2eb59aa344e637
Millenmark <millenmark.iv@gmail.com>
2023-05-10 15:47:10 +0800

    update

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,20 @@ import express from "express"
 dotenv.config()
 connectToDatabase()
 const app = express()
+const port = process.env.PORT || 5000;
+
 
 //middleware
 app.use(express.json())
+
+
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
