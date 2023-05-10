b9e29d32fd43b68fdc6eb4a466f7509c1f2af648
Millenmark <millenmark.iv@gmail.com>
2023-05-10 15:46:34 +0800

    Tried to use prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
-import {Routes, Route} from "react-router-dom"
-import { Navbar } from "./components"
-import './App.css'
-import { ProductsPage } from "./pages"
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
+import "./App.css";
+import { ProductsPage } from "./pages";
 
 function App() {
-
   return (
     <>
       <Navbar />
       <main>
         <Routes>
-          <Route path="/products" element={<ProductsPage/>}/>
+          <Route path="/products" element={<ProductsPage />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
