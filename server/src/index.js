69a943d21dfa667b76ed796ae91ac6ccaa389aae
Millenmark <millenmark.iv@gmail.com>
2023-05-10 16:57:28 +0800

    added the productRoutes

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,24 @@
-import dotenv from "dotenv"
-import connectToDatabase from "./database.js"
-import express from "express"
+import dotenv from "dotenv";
+import connectToDatabase from "./database.js";
+import express from "express";
 
-dotenv.config()
-connectToDatabase()
-const app = express()
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+connectToDatabase();
+const app = express();
 const port = process.env.PORT || 5000;
 
-
 //middleware
-app.use(express.json())
-
-
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
-  res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
