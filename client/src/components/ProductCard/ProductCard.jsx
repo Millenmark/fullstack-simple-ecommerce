fe062548c617a30d5475464ef4e016c90b333a89
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-11 15:32:44 +0800

    Update ProductCard.jsx

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -19,60 +19,103 @@ import { Link as ReactLink} from "react-router-dom"
 import { StarIcon } from "@chakra-ui/icons"
 import { useState } from "react"
 
-const Rating = ({rating, numReviews}) => {
-  const [iconSize, setIconSize] = useState("14px")
+const Rating = ({ rating, numberOfReviews }) => {
+  const [iconSize, setIconSize] = useState("14px");
   return (
     <Flex>
       <HStack spacing={"2px"}>
-        <StarIcon icon={iconSize} w={"14px"} color={"orange.500"}/>
-        <StarIcon icon={iconSize} w={"14px"} color={rating >= 2 ? "orange.500" : "gray.200"}/>
-        <StarIcon icon={iconSize} w={"14px"} color={rating >= 3 ? "orange.500" : "gray.200"}/>
-        <StarIcon icon={iconSize} w={"14px"} color={rating >= 4 ? "orange.500" : "gray.200"}/>
-        <StarIcon icon={iconSize} w={"14px"} color={rating >= 5 ? "orange.500" : "gray.200"}/>
+        <StarIcon icon={iconSize} w={"14px"} color={"orange.500"} />
+        <StarIcon
+          icon={iconSize}
+          w={"14px"}
+          color={rating >= 2 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          icon={iconSize}
+          w={"14px"}
+          color={rating >= 3 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          icon={iconSize}
+          w={"14px"}
+          color={rating >= 4 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          icon={iconSize}
+          w={"14px"}
+          color={rating >= 5 ? "orange.500" : "gray.200"}
+        />
       </HStack>
       <Text fontSize={"md"} fontWeight={"bold"} ml={"4px"}>
-        {`${numReviews} ${numReviews === 1 ? "Review" : "Reviews"}`}
+        {`${numberOfReviews} ${numberOfReviews === 1 ? "Review" : "Reviews"}`}
       </Text>
     </Flex>
-  )
-}
+  );
+};
 
 const ProductCard = ({product}) => {
   return (
-    <Stack 
-      p={"2"} 
-      spacing={"3px"} 
-      bg={useColorModeValue("white", "gray.800")} 
-      minWidth={"240px"} 
-      h={"450px"} 
-      borderWidth={"1px"} 
-      rounded={"lg"} 
-      shadow={"lg"} 
+    <Stack
+      p={"2"}
+      spacing={"3px"}
+      bg={useColorModeValue("white", "gray.800")}
+      minWidth={"240px"}
+      h={"450px"}
+      borderWidth={"1px"}
+      rounded={"lg"}
+      shadow={"lg"}
       position={"relative"}
     >
-      {
-        product.isNew && (<Circle size={"10px"} position={"absolute"} top={"2"} right={"2"} bg={"green.300"}></Circle>)
-      }
-      {
-        product.stock <= 0 && (<Circle size={"10px"} position={"absolute"} top={"2"} right={"2"} bg={"red.300"}></Circle>)
-      }
-      <Image src={product.image} alt={product.name} roundedTop={"lg"}/>
+      {product.isProductNew && (
+        <Circle
+          size={"10px"}
+          position={"absolute"}
+          top={"2"}
+          right={"2"}
+          bg={"green.300"}
+        ></Circle>
+      )}
+      {product.stock <= 0 && (
+        <Circle
+          size={"10px"}
+          position={"absolute"}
+          top={"2"}
+          right={"2"}
+          bg={"red.300"}
+        ></Circle>
+      )}
+      <Image src={product.image} alt={product.name} roundedTop={"lg"} />
       <Box flex={"1"} maxH={"5"} alignItems={"baseline"}>
-        {
-          product.stock <= 0 && (
-            <Badge rounded={"full"} px={"2"} fontSize={"0.8em"} colorScheme={"red"}>Sold Out</Badge>
-          )
-        }
+        {product.stock <= 0 && (
+          <Badge
+            rounded={"full"}
+            px={"2"}
+            fontSize={"0.8em"}
+            colorScheme={"red"}
+          >
+            Sold Out
+          </Badge>
+        )}
 
-        {
-          product.isNew && (
-            <Badge rounded={"full"} px={"2"} fontSize={"0.8em"} colorScheme={"green"}>New</Badge>
-          )
-        }
+        {product.isProductNew && (
+          <Badge
+            rounded={"full"}
+            px={"2"}
+            fontSize={"0.8em"}
+            colorScheme={"green"}
+          >
+            New
+          </Badge>
+        )}
       </Box>
 
       <Flex mt={"1"} justifyContent={"space-between"} alignContent={"center"}>
-        <Link as={ReactLink} to={`/product/${product._id}`} pt={"2"} cursor={"pointer"}>
+        <Link
+          as={ReactLink}
+          to={`/product/${product._id}`}
+          pt={"2"}
+          cursor={"pointer"}
+        >
           <Box fontSize={"2xl"} fontWeight={"semibold"} lineHeight={"tight"}>
             {product.name}
           </Box>
@@ -80,7 +123,10 @@ const ProductCard = ({product}) => {
       </Flex>
 
       <Flex justifyContent={"space-between"} alignContent={"center"} py={"2"}>
-        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <Rating
+          rating={product.rating}
+          numberOfReviews={product.numberOfReviews}
+        />
       </Flex>
 
       <Flex justify={"space-between"}>
@@ -91,14 +137,24 @@ const ProductCard = ({product}) => {
           {product.price.toFixed(2)}
         </Box>
 
-        <Tooltip label={"Add to cart"} bg={"white"} placement={"top"} color={"gray.800"}fontSize={"1.2em"}>
-          <Button variant={"ghost"} display={"flex"} disabled={product.stock <= 0}>
-            <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"}/>
+        <Tooltip
+          label={"Add to cart"}
+          bg={"white"}
+          placement={"top"}
+          color={"gray.800"}
+          fontSize={"1.2em"}
+        >
+          <Button
+            variant={"ghost"}
+            display={"flex"}
+            disabled={product.stock <= 0}
+          >
+            <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
           </Button>
         </Tooltip>
       </Flex>
     </Stack>
-  )
+  );
 }
 
 export default ProductCard
