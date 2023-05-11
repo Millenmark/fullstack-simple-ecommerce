11a6a9419a43337e98bcc77540eb4d951b092c59
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-11 15:10:16 +0800

    created a backendUrl variable

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { setProducts, setError, setLoading } from "../slices/products";
 
+const backendUrl = "http://localhost:5000";
+
 export const getProducts = () => async (dispatch) => {
   dispatch(setLoading(true));
   try {
-    const { data } = await axios.get("/api/products");
+    const { data } = await axios.get(`${backendUrl}/api/products`);
     dispatch(setProducts(data));
   } catch (error) {
     dispatch(
