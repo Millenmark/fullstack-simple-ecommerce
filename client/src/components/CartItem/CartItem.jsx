c3712a55f02ea5a6f3c4b17293055614dc337ee9
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-12 11:35:18 +0800

    Update CartItem.jsx

--- client/src/components/CartItem/CartItem.jsx
+++ client/src/components/CartItem/CartItem.jsx
@@ -30,14 +30,38 @@ const CartItem = ({ cartItem }) => {
           src={image}
           alt={name}
           draggable={"false"}
-          loading={"lazy"}  
+          loading={"lazy"}
         />
-        <Box>
-          <Stack>
-            
+        <Box pt={"4"}>
+          <Stack spacing={"0.5"}>
+            <Text fontWeight={"medium"}>{name}</Text>
           </Stack>
         </Box>
       </Stack>
+      <Flex
+        w={"full"}
+        mt={{ base: "4", md: "0" }}
+        align={{ base: "center", md: "baseline" }}
+        justify={"space-between"}
+        display={"flex"}
+      >
+        <Select
+          maxW={"64px"}
+          focusBorderColor={mode("orange.500", "orange.200")}
+          value={qty}
+          onClick={(e) => {
+            dispatch(id, e.target.value);
+          }}
+        >
+          {[...Array(stock).keys()].map((x) => (
+            <option key={x + 1} value={x + 1}>
+              {x + 1}
+            </option>
+          ))}
+        </Select>
+        <Text fontWeight={"bold"}>${price}</Text>
+        <CloseButton />
+      </Flex>
     </Flex>
   );
 };
