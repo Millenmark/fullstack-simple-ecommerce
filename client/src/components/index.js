be17b56280e4b178794cc465b48cd56144b29053
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-12 13:01:25 +0800

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar/Navbar"
 export { default as ProductCard } from "./ProductCard/ProductCard"
 export { default as CartItem } from "./CartItem/CartItem"
+export { default as CartOrderSummary } from "./CartOrderSummary/CartOrderSummary"
