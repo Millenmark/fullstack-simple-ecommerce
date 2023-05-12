84ed44fdbce3d0042b639716c36fd759366a49b5
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-12 10:53:01 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components";
 import "./App.css";
-import { ProductsPage } from "./pages";
+import { CartPage, ProductsPage } from "./pages";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<CartPage/>}/>
         </Routes>
       </main>
     </>
